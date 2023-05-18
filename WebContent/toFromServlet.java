62fe3959eb048372c661f9346ca63e70b47559a6
purpletv <81405384+purpletv@users.noreply.github.com>
2023-05-18 18:06:36 +0530

    Add files via upload

---
+++ WebContent/toFromServlet.java
@@ -0,0 +1,71 @@
+package TrainManag;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class toFromServlet
+ */
+@WebServlet("/toFromServlet")
+public class toFromServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public toFromServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		Connection c = null;
+		Statement s = null;
+		ResultSet rs = null;
+		String s2 = "";
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection(
+					"jdbc:postgresql://192.168.110.48:5432/plf_training?user=plf_training_admin&password=pff123");
+			s = c.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			rs = s.executeQuery(
+					"select distinct stn_code,stn_name from (select trn_start from gv_trains union all select trn_end from gv_trains) as st,gv_stations as gv where gv.stn_code=st.trn_start  ");
+			rs.absolute(0);
+			while (rs.next()) {
+				s2 += "<option value=" + rs.getString(1) + ">" + rs.getString(2) + "(" + rs.getString(1) + ")"
+						+ "</option>";
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// s1+"</select>";
+		pw.print(s2);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
